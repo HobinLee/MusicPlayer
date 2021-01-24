f22845e4f3078dd6575965bc49502858238e6396
atthemomentkor <at.the.moment.kor@gmail.com>
2021-01-25 02:06:23 +0900

    change background img

--- js/clock.js
+++ js/clock.js
@@ -1,5 +1,5 @@
 const clockContainer = document.querySelector(".js-clock");
-clockTitle = clockContainer.querySelector("h1");
+clockTitle = clockContainer.querySelector("h3");
 
 function getTime() {
   const date = new Date();
