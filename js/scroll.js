e043e6b14c0c60118138ec9f46968b57eb7dd5ce
HobinLee <viny5120@gmail.com>
2021-04-12 23:32:16 +0900

    detect mouse scroll event

--- js/scroll.js
+++ js/scroll.js
@@ -1,18 +1,34 @@
 export class ScrollController {
   constructor() {
-    this._prevScroll = 0;
+    this.MAX_PAGE = 5;
     this._currPage = 1;
     this._scrollPause = true;
-    document.querySelector('.hov-content-wrapper')
-      .addEventListener('mousewheel', (e) => this._scrollPause && this.checkScroll(e));
+    document.addEventListener('scroll', (e) => this._scrollPause && this.checkScroll(e));
   }
   init() {
 
   }
   checkScroll(e) {
-    document.body.qu
+    e.preventDefault;
+    const direction = e.wheelDelta;
     this._scrollPause = false;
+    if (direction < 0) {
+      this._currPage ++;
+    } else {
+      this._currPage --;
+    }
 
+    if (this._currPage < 1) {
+      this._currPage = 1;
+    } else if (this._currPage > this.MAX_PAGE) {
+      this._currPage = this.MAX_PAGE;
+    }
+    this._currPage ++;
+
+    const wrapper = document.querySelector('.hov-content-wrapper');
+    const style = -(this._currPage - 1) * 100 + 'vh;';
+    console.log(style);
+    wrapper.style.top = style;
   }
 }
 
