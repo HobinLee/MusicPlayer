c0f1e7723a69237c57ed1b712588f8ae298b218d
HobinLee <viny5120@gmail.com>
2021-06-13 18:29:47 +0900

    drag control

--- src/components/musicPlayer/MusicController.jsx
+++ src/components/musicPlayer/MusicController.jsx
@@ -1,8 +1,11 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 const MusicController = ({ currentTime, finishTime }) => {
   const { currentMusic } = useSelector(state => state);
+  const [ offset , setOffset ] = useState(null);
+  const [ startMouseX , setStartMouseX ] = useState(0);
+  const [ barSize ] = useState(100 / Number(window.innerWidth > 360 ? 280 : (window.innerWidth - 40)));
 
   useEffect(() => {},[currentTime])
   
@@ -19,6 +22,31 @@ const MusicController = ({ currentTime, finishTime }) => {
     return `${min}:${(sec < 10) ? '0' + sec : sec}`;  
   }
 
+  const initControl = (e) => {
+    if (e.touches.length) {
+      setOffset(Number(e.target.value));
+      setStartMouseX(Number(e.touches[0].clientX));
+    }
+  }
+
+  const handleControl = (e) => {
+    if (e.touches.length) {
+      if (!offset) {
+        setOffset(Number(e.target.value));
+        setStartMouseX(Number(e.touches[0].clientX));
+      } else {
+        const change = (Number(e.touches[0].clientX) - startMouseX) * barSize;
+      
+        let result = offset + change;
+
+        if (result < 0) result = 0;
+        else if (result > 99.99) result = 99.99;
+        
+        handleMusicTime(result);
+      }
+    }
+  }
+
   return (
     <div className = 'hov-music-control-section'>
       <div className = 'hov-music-bar-wrapper'>
@@ -26,7 +54,13 @@ const MusicController = ({ currentTime, finishTime }) => {
               type = "range"
               min = "0" max = "100"
               value = {currentMusic.audio().duration ? (currentMusic.audio().currentTime / currentMusic.audio().duration * 100) : 0}
-              onChange = {(e) => handleMusicTime(e.target.value)}
+              onChange = {(e) => {
+                handleMusicTime(e.target.value);
+              }}
+              onTouchStart = {initControl}
+              onTouchMove = {handleControl}
+
+              onTouchEnd = {() => setOffset(null)}
         />
       </div>
       <div className = 'hov-music-time'>
